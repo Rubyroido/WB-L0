af179001ec740334d96abcf84765e2ab37cd6a8f
Stas Khvan <hvanstas@gmail.com>
2023-09-18 12:14:10 +0300

    feat: сделал основную разметку, добавил шрифты и картинки

---
+++ utils/initialCards.js
@@ -0,0 +1,34 @@
+const initialCards = [
+  {
+    name: 'Футболка UZcotton мужская',
+    color: 'белый',
+    size: '56',
+    storage: 'Коледино WB',
+    seller: 'ООО Вайлдберриз',
+    stock: 2,
+    price: 2500,
+    discount: 15,
+  },
+  {
+    name: 'Силиконовый чехол картхолдер (отверстия) для карт, прозрачный кейс бампер на Apple iPhone XR, MobiSafe',
+    color: 'прозрачный',
+    size: null,
+    storage: 'Коледино WB',
+    seller: 'ООО Вайлдберриз',
+    stock: 250,
+    price: 1000,
+    discount: 20,
+  },
+  {
+    name: 'Карандаши цветные Faber-Castell "Замок", набор 24 цвета, заточенные, шестигранные, Faber-Castell',
+    color: null,
+    size: null,
+    storage: 'Коледино WB',
+    seller: 'ООО Вайлдберриз',
+    stock: 5,
+    price: 1500,
+    discount: 10,
+  }
+]
+
+export default initialCards;
