d69f31091ff81f31891ebfcee38fb2ef0d8f8bc7
Stas Khvan <hvanstas@gmail.com>
2023-09-26 13:25:35 +0300

    feat: сверстал хэдер

--- utils/initialCards.js
+++ utils/initialCards.js
@@ -8,6 +8,7 @@ const initialCards = [
     stock: 2,
     price: 2500,
     discount: 15,
+    isLiked: false
   },
   {
     name: 'Силиконовый чехол картхолдер (отверстия) для карт, прозрачный кейс бампер на Apple iPhone XR, MobiSafe',
@@ -18,6 +19,7 @@ const initialCards = [
     stock: 250,
     price: 1000,
     discount: 20,
+    isLiked: false
   },
   {
     name: 'Карандаши цветные Faber-Castell "Замок", набор 24 цвета, заточенные, шестигранные, Faber-Castell',
@@ -28,6 +30,7 @@ const initialCards = [
     stock: 5,
     price: 1500,
     discount: 10,
+    isLiked: false
   }
 ]
 
